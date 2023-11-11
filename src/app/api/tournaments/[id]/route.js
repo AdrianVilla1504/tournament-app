9dcb834e62d076d55606d30d0751def0d9a5dae7
AdrianVilla1504 <adriancvilla@gmail.com>
2023-11-11 15:50:06 -0500

    feature: finish get and put method for tournaments by params

--- src/app/api/tournaments/[id]/route.js
+++ src/app/api/tournaments/[id]/route.js
@@ -1,16 +1,46 @@
 import { NextResponse } from "next/server";
+import { connectDB } from "@/utils/mongoose";
+import Tournament from "@/models/Tournaments";
 
-export function GET(request, { params }) {
-  console.log(params);
-  return NextResponse.json({
-    message: "Getting single tournament .. ",
-  });
+export async function GET(request, { params }) {
+  try {
+    connectDB();
+    const tournament_found = await Tournament.findById(params.id);
+    if (!tournament_found) {
+      return NextResponse.json(
+        {
+          message: "Tournament not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return NextResponse.json(tournament_found);
+  } catch (error) {
+    return NextResponse.json(error.message, {
+      status: 400,
+    });
+  }
 }
 
-export function PUT(request, { params }) {
-  return NextResponse.json({
-    message: "Updating tournament ...",
-  });
+export async function PUT(request, { params }) {
+  try {
+    const data = await request.json();
+    const updated_tournament = await Tournament.findByIdAndUpdate(
+      params.id,
+      data,
+      {
+        new: true,
+      }
+    );
+    return NextResponse.json({ success: true, updated_tournament });
+  } catch (error) {
+    return NextResponse.json({
+      status: 400,
+    });
+  }
 }
 
 export function DELETE(request, { params }) {
